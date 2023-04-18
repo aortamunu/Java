921f0eccd05dc63aca886c5f9a02450c908600d8
aortamunu <seemi1911@gmail.com>
2023-04-18 18:36:43 +0545

    nw serversocket 01 5 80

--- lab/src/lab1/Login.java
+++ lab/src/lab1/Login.java
@@ -1,24 +1,25 @@
 package lab1;
 import javax.swing.*;
 import java.awt.event.*;
+import java.sql.*;
 
 public class Login implements ActionListener{
     JTextField tfUser;
     JTextField tfPassword;
     JFrame f;
     public Login() {
-        JFrame f = new JFrame("login");
+        f = new JFrame("login");
         JLabel user = new JLabel("Username");
         user.setBounds(20, 20, 150, 20);
         f.add(user);
-        JTextField tfUser = new JTextField();
+        tfUser = new JTextField();
         tfUser.setBounds(180, 20, 150, 20);
         f.add(tfUser);
 
         JLabel password = new JLabel("Password");
         password.setBounds(20, 60, 150, 20);
         f.add(password);
-        JTextField tfPassword = new JTextField();
+        tfPassword = new JTextField();
         tfPassword.setBounds(180, 60, 150, 20);
         f.add(tfPassword);
         JButton b = new JButton("Login");
@@ -36,14 +37,27 @@ public class Login implements ActionListener{
     public void actionPerformed(ActionEvent e){
         String user = tfUser.getText();
         String pwd = tfPassword.getText();
-        if(user.equals("admin")&& pwd.equals("admin")){
-            JDialog d = new JDialog(f, "login success", true);
-            d.setLocationRelativeTo(d);
-            d.setSize(100, 100);
-            d.setVisible(true);
-        }
-
+try{
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ajava_2023","root", "root");
+        Statement smt = con.createStatement();
+        ResultSet rs = smt.executeQuery("select * from User where username = '"+user+"' and password = '"+pwd+"'");
+while(rs.next()) {
+    if (user.equals(rs.getString("username")) && pwd.equals(rs.getString("password"))) {
+        JDialog d = new JDialog(f, "login success", true);
+        d.setLocationRelativeTo(d);
+        d.setSize(100, 100);
+        d.setVisible(true);
     }
 }
+
+    con.close();
+}
+catch(Exception ex){
+    System.out.println(ex.getMessage());
+}
+}
+}
+
 
 
